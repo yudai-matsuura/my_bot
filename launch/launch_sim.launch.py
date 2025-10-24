7fcf48f3aaa9e367935edcb6325f7bc43751a81c
Yudai Matsuura <matsuura.yudai.q5@dc.tohoku.ac.jp>
2025-10-24 21:52:52 +0900

    fix wheel slip issue

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,9 +22,12 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config','gazebo_params.yaml')
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args  --params-file ' + gazebo_params_file}.items()
              )
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
